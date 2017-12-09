c11e09e5d7091f31b80cac12f54460906b1349ca
Tristan Campbell <tristan8campbell@gmail.com>
2017-12-09 14:39:53 +0000

    update config

--- forums/config.php
+++ forums/config.php
@@ -2,11 +2,19 @@
 // phpBB 3.2.x auto-generated configuration file
 // Do not change anything in this file!
 $dbms = 'phpbb\\db\\driver\\mysql';
-$dbhost = 'localhost';
+
+/*$dbhost = 'localhost';
 $dbport = '';
 $dbname = 'tristan';
 $dbuser = 'root';
-$dbpasswd = 'root';
+$dbpasswd = 'root';*/
+
+$dbhost = 'internal-db.s173242.gridserver.com';
+$dbport = '';
+$dbname = 'db173242_tristan';
+$dbuser = 'db173242';
+$dbpasswd = 'dar391092';
+
 $table_prefix = 'phpbb_';
 $phpbb_adm_relative_path = 'adm/';
 $acm_type = 'phpbb\\cache\\driver\\file';
